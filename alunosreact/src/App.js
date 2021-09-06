dd1cb67308a580ad702bd48f37608c029016353e
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2021-09-06 16:31:57 -0300

    Atualizando registro na API

--- alunosreact/src/App.js
+++ alunosreact/src/App.js
@@ -13,6 +13,7 @@ function App() {
 
   const [data, setData] = useState([]);
   const [modalIncluir, setModalIncluir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
   const [alunoSelecionado, setAlunoSelecionado] = useState({
     id: '',
     nome: '',
@@ -20,8 +21,19 @@ function App() {
     idade: ''
   });
 
+  const selecionarAluno = (aluno, opcao) => {
+    setAlunoSelecionado(aluno);
+
+    (opcao === "Editar") &&
+      abrirFecharModalEditar()
+  }
+
   const abrirFecharModalIncluir = () => {
     setModalIncluir(!modalIncluir)
+  }
+
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar)
   }
 
   const handleChange = e => {
@@ -60,6 +72,31 @@ function App() {
       })
   }
 
+  const pedidoPut = async () => {
+
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+
+    await axios.put(baseUrl + "/" + alunoSelecionado.id, alunoSelecionado)
+      .then(response => {
+
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+
+        dadosAuxiliar.map(aluno => {
+          if (aluno.id === alunoSelecionado.id) {
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+        });
+
+        abrirFecharModalEditar();
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
   useEffect(() => {
     pedidoGet();
   })
@@ -90,9 +127,9 @@ function App() {
                 <td>{aluno.email}</td>
                 <td>{aluno.idade}</td>
                 <td>
-                  <button className='btn btn-primary'>Editar</button>
+                  <button className='btn btn-primary' onClick={() => selecionarAluno(aluno, "Editar")}>Editar</button>
                   {' '}
-                  <button className='btn btn-danger'>Excluir</button>
+                  <button className='btn btn-danger' onClick={() => selecionarAluno(aluno, "Excluir")}>Excluir</button>
                 </td>
               </tr>
             ))
@@ -117,6 +154,29 @@ function App() {
         <ModalFooter>
           <button className='btn btn-primary' onClick={() => pedidoPost()}>Incluir</button> {" "}
           <button className='btn btn-danger' onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID: </label>
+            <input type="text" className="form-control" readOnly value={alunoSelecionado && alunoSelecionado.id} /><br />
+
+            <label>Nome: </label>
+            <input type="text" className="form-control" name="nome" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.nome} /><br />
+
+            <label>Email: </label>
+            <input type="text" className="form-control" name="email" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.email} /><br />
+
+            <label>Idade: </label>
+            <input type="text" className="form-control" name="idade" onChange={handleChange} value={alunoSelecionado && alunoSelecionado.idade} /><br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className='btn btn-primary' onClick={() => pedidoPut()}>Editar</button> {" "}
+          <button className='btn btn-danger' onClick={() => abrirFecharModalEditar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
     </div>
